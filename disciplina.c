a61ac6c3aa8995ac3893a87d50c3d5693f236617
jennifer.souza <jenniferocha4@gmail.com>
2025-10-09 14:28:21 -0300

    feat; arquivo separado para as funções de disciplina e funções auxiliares.

--- disciplina.c
+++ disciplina.c
@@ -2,13 +2,6 @@
 #include <string.h>
 #include "definicoes.h"
 
-void limparBuffer() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF);
-}
-
-    
-
 int menuDisciplina(){
     int opcao;
     printf("\n-- Menu Disciplina --\n");
@@ -194,68 +187,4 @@ void excluirDisciplina(Disciplina listaDisc[], int qtdDisc) {
     if (!encontrado) {
         printf("Disciplina com o codigo %d nao encontrada ou ja excluida.\n", codigo);
     }
-}
-
-int validarData(Data data) {
-    // Validação básica
-    if (data.mes < 1 || data.mes > 12 || data.dia < 1 || data.ano <= 0) {
-        return 0;
-    }
-
-    // Validação de dias por mês
-    if (data.mes == 2) { // Fevereiro
-        int bissexto = (data.ano % 4 == 0 && data.ano % 100 != 0) || (data.ano % 400 == 0);
-        if (bissexto) {
-            if (data.dia > 29) return 0;
-        } else {
-            if (data.dia > 28) return 0;
-        }
-    } else if (data.mes == 4 || data.mes == 6 || data.mes == 9 || data.mes == 11) { // Meses com 30 dias
-        if (data.dia > 30) return 0;
-    } else { // Meses com 31 dias
-        if (data.dia > 31) return 0;
-    }
-
-    return 1; // Data válida
-}
-
-int validarCPF(char cpfStr[]) {
-    int cpf[11];
-    int i, j = 0;
-
-   
-    for (i = 0; cpfStr[i] != '\0'; i++) {
-        if (cpfStr[i] >= '0' && cpfStr[i] <= '9') {
-            if (j < 11) {
-                cpf[j++] = cpfStr[i] - '0';
-            }
-        }
-    }
-
-    if (j != 11) return 0; 
-
-    for (i = 0; i < 10; i++) {
-        if (cpf[i] != cpf[i + 1]) break;
-    }
-    if (i == 10) return 0;
-
-    // Primeiero dígito verificador
-    int soma = 0;
-    for (i = 0, j = 10; i < 9; i++, j--) {
-        soma += cpf[i] * j;
-    }
-    int resto = (soma * 10) % 11;
-    if (resto == 10) resto = 0;
-    if (resto != cpf[9]) return 0;
-
-    // Segundo dígito verificador
-    soma = 0;
-    for (i = 0, j = 11; i < 10; i++, j--) {
-        soma += cpf[i] * j;
-    }
-    resto = (soma * 10) % 11;
-    if (resto == 10) resto = 0;
-    if (resto != cpf[10]) return 0;
-
-    return 1;
 }
